f06d60f88933ff993b5c6e39c168e04d3ce03ecb
adrian <thenewmailingadress@gmail.com>
2022-06-08 13:16:35 -0500

    06-08-2022

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Comic Mono:size=13" };
-static const char dmenufont[]       = "Comic Mono:size=13";
+static const char *fonts[]          = { "Hack:size=13" };
+static const char dmenufont[]       = "Hack:size=13";
 #include "/home/adrian/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
